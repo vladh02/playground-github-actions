d27ac4110cb77a8273349d3991f4ce2c49065948
vlad <vladhstudios@gmail.com>
2020-09-21 19:44:54 +0200

    add dummy app

---
+++ index.js
@@ -0,0 +1,11 @@
+const os = require("os");
+const http = require("http");
+
+const handleRequest = (req, res) => {
+  res.write(`Hi there! I'm being served from ${os.hostname()} \n\n`);
+  res.end();
+};
+
+http.createServer(handleRequest).listen(3000);
+
+module.exports = romanNumeralTranslator;
