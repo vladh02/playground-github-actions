5f1d82c467add0ad827ce2245fd3ce49f5f3f22e
vlad <vladhstudios@gmail.com>
2020-09-22 12:03:11 +0200

    nvm use

--- setup_server.sh
+++ setup_server.sh
@@ -7,3 +7,7 @@ curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | b
 nvm install 12.18.4
 
 nvm use 12.18.4
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
