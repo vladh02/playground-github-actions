de71675efab1a9dcd44229693254f98077754002
Vlad Haiduc <vladhstudios@gmail.com>
2020-09-23 21:53:27 +0200

    update setup server script

--- setup_server.sh
+++ setup_server.sh
@@ -2,13 +2,23 @@
 
 set -e
 
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-
 main() {
-    exec bash
     nvm install 12.18.4
     nvm use 12.18.4
 }
 
+NVM_DIR="$HOME/mynvm"
+git clone https://github.com/nvm-sh/nvm.git "$NVM_DIR"
+pushd  "$NVM_DIR"
+git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --tags --max-count=1)`
+
+popd
+chmod +x "$NVM_DIR/nvm.sh"
+. "$NVM_DIR/nvm.sh" --no-use
+
+
 main
+
+echo "SETUP_NODE_NVM_NVM: $NVM_DIR/nvm.sh"
+echo "SETUP_NODE_NVM_NODE: $(which node)"
+echo "SETUP_NODE_NVM_NPM: $(which npm)"
