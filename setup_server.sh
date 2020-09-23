46600658c04e551fc3f29a0de857b4a615889ad3
Vlad Haiduc <vladhstudios@gmail.com>
2020-09-23 21:36:01 +0200

    remove not needed export

--- setup_server.sh
+++ setup_server.sh
@@ -9,7 +9,3 @@ source ~/.bashrc
 nvm install 12.18.4
 
 nvm use 12.18.4
-
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
