14a90160c8ac354ecb3f9be93c0a636035b482e7
Vlad Haiduc <vladhstudios@gmail.com>
2020-09-23 21:59:18 +0200

    cleanup directory

--- setup_server.sh
+++ setup_server.sh
@@ -8,6 +8,7 @@ main() {
 }
 
 NVM_DIR="$HOME/mynvm"
+rm -rf "$NVM_DIR"
 git clone https://github.com/nvm-sh/nvm.git "$NVM_DIR"
 pushd  "$NVM_DIR"
 git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --tags --max-count=1)`
