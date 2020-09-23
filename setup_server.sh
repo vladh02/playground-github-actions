808fde4c697672f408182254e682c39c283ca298
Vlad Haiduc <vladhstudios@gmail.com>
2020-09-23 21:44:55 +0200

    update nvm

--- setup_server.sh
+++ setup_server.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
 set -e
+set -o pipefail
 
-nvm install 12.18.4
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 
-nvm use 12.18.4
+main() {
+    exec bash
+    nvm install 12.18.4
+    nvm use 12.18.4
+}
+
+main
